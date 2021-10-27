11c5c2b83a97fb34cec564fd080713ea740037d7
kamakshiashiashokkumar <kamakshiashokkumar@gmail.com>
2021-10-27 13:26:38 +0530

    second commit

---
+++ test.sh
@@ -0,0 +1,2 @@
+this file for the git revert command
+and we have to seen the how it will work
--- wallmart1.java
+++ wallmart1.java
@@ -1,3 +1,4 @@
 hello
 this is the first file in this applications
-we need to learn more 
+we need to learn more
+this is the updated info
